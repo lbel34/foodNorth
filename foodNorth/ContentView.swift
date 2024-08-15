43a85f373d5f9bf1a7e67cf4c9a944552184c186
Lena <lbel342092gmail.com>
2024-08-15 13:10:48 +0100

    north london food

--- foodNorth/ContentView.swift
+++ foodNorth/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Sustainable food places North London")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            Spacer()
+            Text("Plants of Roselyn, 102 Tooley St, London SE1 2TH")
+            Spacer ()
+            Text("Miranda Café, 28 Broadway Parade, London N8 9DB")
+            Spacer ()
+            Text("Jam Delish Islington, 1 Tolpuddle St, London N1 0XT")
+            Spacer ()
+            Text ("Acme Fire Cult, Dalston, The Bootyard, Abbot St, London E8 3DP")
+            Spacer ()
+            Text ("Barge East, Hackney Wick,White Post Lane, E9 5EN")
+            Spacer ()
+            Text ("EDIT, Hackney,217 Mare Street, E8 3QE")
+            Spacer ()
+            Text ("Zero Zero Restaurants, 2nd Floor, O2 Centre, 255 Finchley Rd, London NW3 6LU")
+            Spacer ()
+            Text ("Casa Fofó, 158 Sandringham Rd, London E8 2HS")
+
+            
         }
-        .padding()
     }
 }
 
