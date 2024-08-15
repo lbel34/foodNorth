22a520b08394157479227171bc1ea331533a0f8a
Lena <lbel342092gmail.com>
2024-08-15 14:53:43 +0100

    foodNorth

--- foodNorth/ContentView.swift
+++ foodNorth/ContentView.swift
@@ -16,20 +16,42 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
             Spacer()
             Text("Plants of Roselyn, 102 Tooley St, London SE1 2TH")
+                .fontWeight(.bold)
+                
+                
+            Text ("Traditional Thai cookery incorporates plenty of plant-based dishes that we enjoy daily at home")
+                
             Spacer ()
             Text("Miranda Café, 28 Broadway Parade, London N8 9DB")
+                .fontWeight(.bold)
+            Text ("Cafe that focuses on organic and sustainable food")
             Spacer ()
             Text("Jam Delish Islington, 1 Tolpuddle St, London N1 0XT")
-            Spacer ()
+                .fontWeight(.bold)
+            Text ("Vegan Carribean restaurant located in the heart of Angel, London")
+    
+          Spacer ()
             Text ("Acme Fire Cult, Dalston, The Bootyard, Abbot St, London E8 3DP")
+                .fontWeight(.bold)
+            Text ("The menu focuses on organic and heirloom ingredients sourced micro-seasonally, across the UK. At least 50% of the menu is plant-based.")
+                  
+            
             Spacer ()
             Text ("Barge East, Hackney Wick,White Post Lane, E9 5EN")
+                .fontWeight(.bold)
+            Text ("Ingredients sourced from our gardens and sustainable suppliers")
             Spacer ()
             Text ("EDIT, Hackney,217 Mare Street, E8 3QE")
+                .fontWeight(.bold)
+            Text ("Ingredients are selected from small-scale farms, foragers and independent producers who use regenerative and minimal intervention methods")
             Spacer ()
             Text ("Zero Zero Restaurants, 2nd Floor, O2 Centre, 255 Finchley Rd, London NW3 6LU")
+                .fontWeight(.bold)
+            Text ("Zero waste Italian restaurant")
             Spacer ()
             Text ("Casa Fofó, 158 Sandringham Rd, London E8 2HS")
+                .fontWeight(.bold)
+            Text ("Set menu featuring seasonal ingredients sourced by local producers and suppliers")
 
             
         }
