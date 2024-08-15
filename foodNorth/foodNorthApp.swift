ca21ab90ab720355a8d22bcd452de4a786e1674e
Lena <lbel342092gmail.com>
2024-08-15 12:00:03 +0100

    Initial Commit

---
+++ foodNorth/foodNorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodNorthApp.swift
+//  foodNorth
+//
+//  Created by Scholar on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct foodNorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
